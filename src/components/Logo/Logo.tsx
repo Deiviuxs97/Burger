eabf4b784b277dc84a53ac4a8aa9e9ec7b43ee4a
Deividas <deiviuxs97@gmail.com>
2020-07-10 16:53:22 +0300

    8-29 nebaigtas

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,8 +3,10 @@ import React from "react";
 import burgerLogo from "../../assets/images/burger-logo.png";
 import classes from "./Logo.module.css";
 
-const logo = () => (
-  <div className={classes.Logo}>
+interface props {}
+
+const logo = (props: { height: any }) => (
+  <div style={{ height: props.height }}>
     <img src={burgerLogo} alt="myBurger" />
   </div>
 );
