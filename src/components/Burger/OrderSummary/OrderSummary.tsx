17c73dfd3d87ec8cb0c9a245ada7def8ed32325c
Deividas <deiviuxs97@gmail.com>
2020-07-10 14:16:55 +0300

    8-22

--- src/components/Burger/OrderSummary/OrderSummary.tsx
+++ src/components/Burger/OrderSummary/OrderSummary.tsx
@@ -21,6 +21,8 @@ const orderSummary = (props: props) => {
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   );
 };
---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import classes from "./Button.module.css";
+
+const button = (props: {
+  btnType: React.Key;
+  clicked:
+    | ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void)
+    | undefined;
+  children: React.ReactNode;
+}) => (
+  <button
+    className={[classes.Button, classes[props.btnType]].join(" ")}
+    onClick={props.clicked}
+  >
+    {props.children}
+  </button>
+);
+
+export default button;
